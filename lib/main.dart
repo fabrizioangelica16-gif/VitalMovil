aaf12e5e9742126a0acd7feb5cf48ead0391dfd3
fabrizioangelica16-gif <fabrizio.angelica16@gmail.com>
2026-06-22 21:15:34 -0300

    Update main.dart with provider

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'screens/home_screen.dart';
+import 'providers/mascota_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const VitalMovilApp());
+}
+
+class VitalMovilApp extends StatelessWidget {
+  const VitalMovilApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MascotaProvider()),
+      ],
+      child: MaterialApp(
+        title: 'VitalMovil',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: const HomeScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
